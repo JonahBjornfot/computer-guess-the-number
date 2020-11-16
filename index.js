7783e4cbddf56cc43a723de6a6712cb55a232bf5
root <root@Jonah.localdomain>
2020-11-16 15:47:24 +0100

    funkardåligt

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const MAX_NUMBER = 100
-const MIN_NUMBER = 0
-
-let guess
-
-document.getElementById('too-high-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too high?
-
-})
-
-document.getElementById('too-low-btn').addEventListener('click', function () {
-    // Vad ska hända när man tryckt too low?
-    
-})
-
+let guess = Math.ceil(Math.random() * 99)
+let minus = guess - 1;
+let plus = guess + 1;
 
 function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
-
-    guess = 0
-
-    // setMessage(`Is it ${guess}?`)
+    document.getElementById("message").innerHTML = "Är det" + " " + guess + "?"
 }
 
-document
-    .getElementById('start-btn')
-    .addEventListener('click', start)
+const hög = document.getElementById('too-high-btn').addEventListener('click', function () {
+    document.getElementById("message").innerHTML = "Är det" + " " + minus + "?"
+})
 
+const låg = document.getElementById('too-low-btn').addEventListener('click', function () {
+    document.getElementById("message").innerHTML = "Är det" + " " + plus + "?"
+})
+
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    const äntligen = prompt("Hur många försök?")
+    console.log(äntligen)
+})
+
+document.getElementById("Reload").addEventListener('click', function () {
+    location.reload();
+})
+document.getElementById('start-btn').addEventListener('click', start)
+
+// var button = document.getElementById("clickme"),
+//   count = 0;
+// button.onclick = function() {
+//   count += 1;
+//   button.innerHTML = "Click me: " + count;
+// };
